e2a035d38c9c5549010498f0ad7c34d430ff1e65
Owen Beck <oebeck@gmail.com>
2022-04-19 18:02:55 -0400

    Updated ports.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,4 +3,8 @@ package frc.robot;
 public class Ports {
     public static int SHOOTER_MOTOR_LEFT = 1;
     public static int SHOOTER_MOTOR_RIGHT = 2;
+    public static int RightDriveFalconMainCAN = 3;
+    public static int LeftDriveFalconMainCAN = 4;
+    public static int RightDriveFalconSubCAN = 5;
+    public static int LeftDriveFalconSubCAN = 6;
 }
