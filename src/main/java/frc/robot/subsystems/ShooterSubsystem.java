a3ee58be6ab313c005ce2ada4fdebae2b6ecc30c
Rgamer43 <54045197+Rgamer43@users.noreply.github.com>
2022-05-03 15:55:39 -0400

    Shooter should have pID now.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PidParameters;
 import frc.robot.Ports;
 import frc.robot.TeamSparkMAX;
 
@@ -11,6 +12,19 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private static double voltage = 12;
 
+    private PidParameters pidParameters;
+    private static double kP = .0005,
+        kI = 0,
+        kD = 0,
+        kF = .00018,
+        kIZone = 0,
+        kPeakOutput = 1,
+        maxVel = 20000,
+        maxAcc = 1500;
+    private static int errorTolerance = 10;
+
+    private static int maximumRPM = 5600;
+
     public ShooterSubsystem() {
         leftMotor = new TeamSparkMAX("Left Shooter Motor", Ports.SHOOTER_MOTOR_LEFT);
         rightMotor = new TeamSparkMAX("Right Shooter Motor", Ports.SHOOTER_MOTOR_RIGHT);
@@ -19,6 +33,8 @@ public class ShooterSubsystem extends SubsystemBase {
 
         leftMotor.enableVoltageCompensation(voltage);
         rightMotor.enableVoltageCompensation(voltage);
+
+        pidParameters = new PidParameters(kP, kI, kD, kF, kIZone, kPeakOutput, maxVel, maxAcc, errorTolerance);
     }
 
     public static ShooterSubsystem Create() {
@@ -26,7 +42,20 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void set(float power, String reason) {
-        leftMotor.set(power, reason);
+        leftMotor.setSmartMotionVelocity(powerToRPM(power), reason);
+        leftMotor.configureWithPidParameters(pidParameters, 0);
+        rightMotor.configureWithPidParameters(pidParameters, 0);
+    }
+
+    public double powerToRPM(double power) {
+        return Math.max(-1, Math.min(1, power)) * maximumRPM;
+    }
+
+    public void periodic() {
+        leftMotor.periodic();
+        rightMotor.periodic();
+        pidParameters.periodic("Subsystems.Shooter.Left", leftMotor, 0);
+        pidParameters.periodic("Subsystems.Shooter.Right", rightMotor, 0);
     }
     
 }
