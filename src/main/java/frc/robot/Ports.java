0ae02fbd0323d419e0d0ba46bf1b83b4c5372ecb
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-04-11 17:58:57 -0400

    Updated ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,8 +1,8 @@
 package frc.robot;
 
 public class Ports {
-    public static int SHOOTER_MOTOR_LEFT = 1;
-    public static int SHOOTER_MOTOR_RIGHT = 2;
+    public static int SHOOTER_MOTOR_LEFT = 17;
+    public static int SHOOTER_MOTOR_RIGHT = 1;
     public static int RightDriveFalconMainCAN = 3;
     public static int LeftDriveFalconMainCAN = 4;
     public static int RightDriveFalconSubCAN = 5;
